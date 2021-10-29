6f309095e275cd5900a3b633340706ad5cbdc94a
robonexx <robertwagar@gmail.com>
2021-10-29 10:55:37 +0200

    Playing with state... ;D

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,28 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 
 const Home = () => {
+    const [name, setName] = useState('Rob');
+    
+
+    const handleClick = () => {
+        setName(prompt('What is your name'))
+    };
     return (
-       <h1>Home</h1>
-     );
-}
- 
+        <div className="home">
+            {(name === 'Rob') ?
+                <div>
+                    <h1> Welcome to my practice</h1>
+                    <h1>My name is {name}</h1>
+                </div> :
+                <div>
+                   <h1> Welcome <span className="highlight">{name}</span> nice to have you here</h1>
+                </div>
+               
+            }
+
+      <button className="btn" onClick={handleClick}>What is your name?</button>
+    </div>
+  );
+};
+
 export default Home;
