16bd2460678e551095daf0a8f61e2a3c20f5966b
robonexx <robertwagar@gmail.com>
2021-10-29 11:09:56 +0200

    practice state changes and mapping array with objects

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 
 const Home = () => {
     const [name, setName] = useState('Rob');
+    const [events, setEvents] = useState([
+        { title: "End racism and opression", id: 1 },
+        { title: "Take care of nature, pick up all your trash", id: 2 },
+        {title: "End violence and control gun violence", id: 3}
+    ])
     
 
     const handleClick = () => {
@@ -16,11 +21,20 @@ const Home = () => {
                 </div> :
                 <div>
                    <h1> Welcome <span className="highlight">{name}</span> nice to have you here</h1>
-                </div>
-               
+                </div>  
             }
 
-      <button className="btn" onClick={handleClick}>What is your name?</button>
+            <button className="btn" onClick={handleClick}>What is your name?</button>
+            
+            <div className="lower">
+                So what are the state of the world? we need to change
+            </div>
+
+            {events.map((ev) => (
+                <div key={ev.id}>
+                    <h2 className="events">{ev.title}</h2>
+                </div>
+            ))}
     </div>
   );
 };
